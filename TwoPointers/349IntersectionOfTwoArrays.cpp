c92bf246022e869d5eb0eb1b0e630d933cffa10b
Ying <48667135+laurayinglu@users.noreply.github.com>
2020-03-16 17:36:18 -0500

    Create 349IntersectionOfTwoArrays.cpp

---
+++ TwoPointers/349IntersectionOfTwoArrays.cpp
@@ -0,0 +1,12 @@
+// Given two arrays, write a function to compute their intersection.
+
+// example: 
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2]
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        
+    }
+};
