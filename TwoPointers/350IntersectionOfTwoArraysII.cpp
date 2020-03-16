c3a3cb72f82a7409830041d7e6bc0d3a0ca3a357
Ying <48667135+laurayinglu@users.noreply.github.com>
2020-03-16 18:31:33 -0500

    Update 350IntersectionOfTwoArraysII.cpp

--- TwoPointers/350IntersectionOfTwoArraysII.cpp
+++ TwoPointers/350IntersectionOfTwoArraysII.cpp
@@ -10,8 +10,37 @@ What if elements of nums2 are stored on disk, and the memory is limited such tha
 */
 
 class Solution {
+
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        
+        vector<int> res;
+
+        if(nums1.size() == 0 || nums2.size() == 0) return res;
+
+        // sort both vectors
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int l = 0;
+        int s = 0;
+
+        while(l < nums1.size() && s < nums2.size())
+        {
+            if(nums1[l] > nums2[s]) {
+                s++;
+                continue;}
+            if(nums1[l] < nums2[s]){
+                l++;
+                continue;}
+
+            res.push_back(nums1[l]);
+            s++;
+            l++;
+
+        }
+
+        return res;
     }
+    
 };
+
