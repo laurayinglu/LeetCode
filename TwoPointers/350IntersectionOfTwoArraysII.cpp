6ad90e7dcf9f0339828f52745b74e3790a6a6b6e
Ying <48667135+laurayinglu@users.noreply.github.com>
2020-03-16 18:55:22 -0500

    Update 350IntersectionOfTwoArraysII.cpp

--- TwoPointers/350IntersectionOfTwoArraysII.cpp
+++ TwoPointers/350IntersectionOfTwoArraysII.cpp
@@ -4,9 +4,14 @@
 
 Follow up:
 
-What if the given array is already sorted? How would you optimize your algorithm?
-What if nums1's size is small compared to nums2's size? Which algorithm is better?
-What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+- What if the given array is already sorted? How would you optimize your algorithm?
+1) Classic two pointer problem
+
+- What if nums1's size is small compared to nums2's size? Which algorithm is better?
+1) 
+
+- What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+1) Map-Reduce (maybe)
 */
 
 class Solution {
@@ -43,4 +48,22 @@ public:
     }
     
 };
+// time: O(nlogn)
+// space: O(1)
+// we can also use hashmap to record frequencies
 
+// equivalent way
+  sort(a.begin(), a.end()); 
+  sort(b.begin(), b.end());
+  a.erase(set_intersection(a.begin(), a.end(), b.begin(),b.end(), a.begin()), a.end());
+  return a;
+
+// use hashmap version
+  unordered_map<int, int> ctr;
+    for (int i : a)
+        ctr[i]++;
+    vector<int> out;
+    for (int i : b)
+        if (ctr[i]-- > 0)
+            out.push_back(i);
+    return out;
