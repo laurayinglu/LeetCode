06bad1d4f6c5b0d6c2b696cadcb8121bd0c8437e
Ying <48667135+laurayinglu@users.noreply.github.com>
2020-03-16 13:00:17 -0500

    Update 977SquaresOfASortedArray.cpp

--- TwoPointers/977SquaresOfASortedArray.cpp
+++ TwoPointers/977SquaresOfASortedArray.cpp
@@ -4,14 +4,29 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& A) {
-    
-    
-    
-    
-    
-    
-    
-    
+        // two pointers
+        vector<int> res;
         
+        for(int i = 0; i<A.size(); i++)
+            res.push_back(A[i]);
+        
+        int size = A.size()-1;
+        int i = 0;
+        int j = size;
+        
+        for(int k = size; k >= 0; k--)
+        {
+            if(abs(A[i]) > abs(A[j])){
+                res[k] = pow(A[i],2);
+                i++;
+            }
+            else{
+                res[k] = pow(A[j],2);
+                j--;
+            }
+        }
+        
+        return res;
+         
     }
 };
