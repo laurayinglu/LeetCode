79c26d6ab23ce4bb6f7a3433f54f30706fb4359a
Ying <48667135+laurayinglu@users.noreply.github.com>
2020-03-17 17:53:02 -0500

    Update 167TwoSumII.cpp

--- TwoPointers/167TwoSumII.cpp
+++ TwoPointers/167TwoSumII.cpp
@@ -17,7 +17,24 @@ Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        
+        int l = 0;
+        int r = numbers.size()-1;
+        while(l<=r)
+        {
+            if(numbers[l]+numbers[r]==target)
+            {
+                vector<int> res{l+1,r+1};
+                return res;
+            }
+            
+            if(numbers[l]+numbers[r] < target)
+                l++;
+            else if (numbers[l]+numbers[r] > target)
+                r--;        
+        }
+        return {};  
     }
 };
+
+// Use two-pointers
 
